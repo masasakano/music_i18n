d4ab570dc64c292168da42e36f2297093f79fbfb
Masa Sakano <masasakano@users.noreply.github.com>
2023-10-02 04:40:19 +0100

    Refactored Dropdown-related JavaScript, removing 4 files.
    * `app/javascript/dropdown_MODEL.js` unified into `dropdown_country2place_all.js`

--- app/javascript/dropdown_country2place_all.js
+++ app/javascript/dropdown_country2place_all.js
@@ -1,12 +1,10 @@
 // Place-Prefecture-Country Dropdown menu for models
-//
-// 2 change() functions are defined.
 
 // console.log('dropdown JS file is going to be read.');
 
 import {dropdownCountry2Place} from './dropdown_country2place.js';
 
-const models = ["event_group"];
+const models = ["artist", "event_group", "harami_vid", "music", "place"];
 
 models.forEach(dropdownCountry2Place);
 
